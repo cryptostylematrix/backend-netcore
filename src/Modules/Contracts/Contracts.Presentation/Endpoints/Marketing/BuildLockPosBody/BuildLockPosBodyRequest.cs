4598e5aa81296b40392d29b8884c047323a1a4c4
Crypro Style <240541670+cryptostylematrix@users.noreply.github.com>
2026-05-26 15:04:37 +0100

    change lock_pos and unlock_pos endpoint params

--- src/Modules/Contracts/Contracts.Presentation/Endpoints/Marketing/BuildLockPosBody/BuildLockPosBodyRequest.cs
+++ src/Modules/Contracts/Contracts.Presentation/Endpoints/Marketing/BuildLockPosBody/BuildLockPosBodyRequest.cs
@@ -2,8 +2,15 @@ namespace Contracts.Presentation.Endpoints.Marketing.BuildLockPosBody;
 
 public sealed class BuildLockPosBodyRequest
 {
+    [BindFrom("m")]
     public int M { get; init; }
+    
+    [BindFrom("profile_addr")]
     public string ProfileAddr { get; init; } = null!;
+    
+    [BindFrom("parent_addr")]
     public string ParentAddr { get; init; } = null!;
+    
+    [BindFrom("pos")]
     public int Pos { get; init; }
 }
--- src/Modules/Contracts/Contracts.Presentation/Endpoints/Marketing/BuildUnlockPosBody/BuildUnlockPosBodyRequest.cs
+++ src/Modules/Contracts/Contracts.Presentation/Endpoints/Marketing/BuildUnlockPosBody/BuildUnlockPosBodyRequest.cs
@@ -2,8 +2,15 @@ namespace Contracts.Presentation.Endpoints.Marketing.BuildUnlockPosBody;
 
 public sealed class BuildUnlockPosBodyRequest
 {
+    [BindFrom("m")]
     public int M { get; init; }
+    
+    [BindFrom("profile_addr")]
     public string ProfileAddr { get; init; } = null!;
+    
+    [BindFrom("parent_addr")]
     public string ParentAddr { get; init; } = null!;
+    
+    [BindFrom("pos")]
     public int Pos { get; init; }
 }
